0214795c1d682e4ba153e96de6e6790d9d405db8
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-01 20:30:09 -0300

    tipos de variaveis em java

--- JAVA/A01teste.java
+++ JAVA/A01teste.java
@@ -2,5 +2,17 @@ public class A01teste{
     public static void main(String[] args) {
         //tipos primitivos em JAVA
         //int, double, float, char, byte, short, long, boolean
-    }
+        int age = 201;
+        long numeroGrande = 100000;
+        double salarioDouble = 1500;
+        float salarioFloat = 2000;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'K';
+            System.out.println(age + salarioDouble);
+            System.out.println(salarioDouble);
+            System.out.println("isso ae é meu salario po mas adiciona uns zeros");
+}
 }

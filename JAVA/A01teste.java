a4f1d64370316df84a7d8394e3b7398beda0f972
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-01 19:35:57 -0300

    tipos primitivos JAVA

---
+++ JAVA/A01teste.java
@@ -0,0 +1,6 @@
+public class A01teste{
+    public static void main(String[] args) {
+        //tipos primitivos em JAVA
+        //int, double, float, char, byte, short, long, boolean
+    }
+}
--- JAVA/Teste2.java
+++
@@ -1,5 +0,0 @@
-public class Teste2 {
-    public static void main(String[] args){
-        System.out./* isso é um comentario de munltiplas linhas*/println("aaaaa");
-    }
-}

81be86b92cd39742e3c3efb3092e18c4c721d92d
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-02 18:05:40 -0300

    operadoresp1 fechou

---
+++ JAVA/A04Operadores.java
@@ -0,0 +1,10 @@
+public class A04Operadores {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        double numero2= 20;
+        double coiso = numero1 / numero2;
+
+    // + - / *
+        System.out.println(coiso );
+    }
+}

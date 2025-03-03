c10cd80a92db94137c456dd7fb42e53c09a9767c
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-02 22:21:46 -0300

    Condicionais IF java

--- JAVA/A6Condicionais.java
+++ JAVA/A6Condicionais.java
@@ -3,12 +3,20 @@ public class A6Condicionais {
     public static void main(String[] args) {
 
         int idade = 16;
+        boolean isAutorizadoComprarBebida = idade >= 18;
 
-        if (idade >= 18){
+        if (isAutorizadoComprarBebida){
             System.out.println("pode comprar bebida");
         } 
+
+        //!
             
-        System.out.println("pode n man ");
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Você não esta autorizado a comprar bebida!!");
+            
+        }
+
+        
     }
     
 }

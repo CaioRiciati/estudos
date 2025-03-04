a8e2182baf8ac615d7d0404026b2be0b1c4c0447
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-04 14:44:12 -0300

    breakLaço em java

---
+++ JAVA/A13RepeticãoExercicio.java
@@ -0,0 +1,14 @@
+public class A13RepeticãoExercicio {
+    public static void main(String[] args) {
+        
+   
+    int miao = 0;
+
+    for(miao = 0;miao <1000 ; miao++){
+        if (miao % 2 == 0) {
+            System.out.println(miao);
+        
+         }
+        }
+    }
+}

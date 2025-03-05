2cb332ad3d37bfca7af5bcc16d7d12d8c0b90bed
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-04 22:00:30 -0300

    break e continie fechou

--- JAVA/A15ExercicioBreak.java
+++ JAVA/A15ExercicioBreak.java
@@ -4,13 +4,13 @@ public class A15ExercicioBreak {
     //dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
     //condição valorParcela >= 1000
     public static void main(String[] args) {
-    double valorCaroo = 40000;
+    double valorCaroo = 4000;
 
-        for(int parcela = 1; parcela<= valorCaroo; parcela++){
+        for(int parcela = 1; parcela <= valorCaroo; parcela++){
             double valorParcela = valorCaroo / parcela;
             if (valorParcela >= 1000) {
-                System.out.println("parcela "+ parcela+ " R$ "+valorParcela );
             }
+            System.out.println("parcela "+ parcela+ " R$ "+valorParcela );
 
         }
 

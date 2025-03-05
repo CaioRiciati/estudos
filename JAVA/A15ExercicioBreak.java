42f762a91f81034b13d6050cd9f2e81541f28a00
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-05 12:21:01 -0300

    Aprendi a usar for no exercicio break

--- JAVA/A15ExercicioBreak.java
+++ JAVA/A15ExercicioBreak.java
@@ -4,13 +4,14 @@ public class A15ExercicioBreak {
     //dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
     //condição valorParcela >= 1000
     public static void main(String[] args) {
-    double valorCaroo = 4000;
+    double valorCarro = 100000;
 
-        for(int parcela = 1; parcela <= valorCaroo; parcela++){
-            double valorParcela = valorCaroo / parcela;
-            if (valorParcela >= 1000) {
+        for(int parcela = 1; parcela <= valorCarro; parcela++){
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela <= 1000) {
+            break;
             }
-            System.out.println("parcela "+ parcela+ " R$ "+valorParcela );
+            System.out.println("parcela "+ parcela+ "x de R$ "+valorParcela );
 
         }
 
--- JAVA/A17Arrays.java
+++ JAVA/A17Arrays.java
@@ -1,5 +1,6 @@
 public class A17Arrays {
     public static void main(String[] args) {
-
+        
+        int [] idades;
     }
 }

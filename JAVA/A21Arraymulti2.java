df83541b8d899c189e0b0e386112bb8c2ba33f76
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-07 00:01:26 -0300

    hj fechou

---
+++ JAVA/A21Arraymulti2.java
@@ -0,0 +1,8 @@
+public class A21Arraymulti2 {
+    public static void main(String[] args) {
+         int[][] valor1 = {{2,4,1},{4,8,6}};
+         int[][] valor2 = {{4,4,2},{3,2,6}};
+        }
+    }
+    
+}

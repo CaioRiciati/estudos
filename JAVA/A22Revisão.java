894b470e50d0ae47369c173e8b4d7c0cdc79e28c
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-08 21:56:19 -0300

    Revisões em java
    estava revisando sobre arrays pq n tinha entendido mt bem

---
+++ JAVA/A22Revisão.java
@@ -0,0 +1,21 @@
+public class A22Revisão {
+    public static void main(String[] args) {
+        int[] numeros = {2,6,1,4,3,6,3,4,6,8};
+        int soma = 0;
+
+
+            
+        
+        for (int i = 0; i < numeros.length; i++) { 
+            if (numeros [i] % 2 == 0) {
+            soma += numeros[0];
+            }
+        }
+    
+    
+
+
+        System.out.println("A soma dos números pares é: " + soma);
+        
+    }
+}
---
+++ JAVA/A22Revisão3.java
@@ -0,0 +1,9 @@
+public class A22Revisão3 {
+    public static void main(String[] args) {
+        char[] vogal = {'a', 'e', 'i', 'o', 'u'};
+        for (int i = 0; i < vogal.length; i++) {
+
+        }
+    }
+    
+}

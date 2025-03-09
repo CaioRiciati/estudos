a832be14eb934dc06bf71aedfac2df457e2e85cd
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-09 13:20:46 -0300

    realoquei Java

--- JAVA/A22Revisão3.java
+++
@@ -1,9 +0,0 @@
-public class A22Revisão3 {
-    public static void main(String[] args) {
-        char[] vogal = {'a', 'e', 'i', 'o', 'u'};
-        for (int i = 0; i < vogal.length; i++) {
-
-        }
-    }
-    
-}
--- JAVA/a02SLA.java
+++
@@ -1,8 +0,0 @@
-public class a02SLA {
-    public static void main(String[] args) {
-        short idade = 16;
-        String nome = "caio";
-        var noseuque = "123";
-        System.out.println("meu nome é " + nome + " e eu tenho " +idade+ " anos");
-    }
-}
